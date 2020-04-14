be6c44d10bcd0bdfe3ce281c1da6476ac1c106a7
Johann Gerell <johann@gerell.net>
2020-04-14 17:49:25 +0200

    Minor change in the jg::stacktrace sample

--- samples/jg_stacktrace.cpp
+++ samples/jg_stacktrace.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <jg_stacktrace.h>
 
+auto get_trace()
+{
+    return jg::stack_trace()
+               .include_frame_count(10) // actual count can be less
+               .skip_frame_count(1) // skips get_trace()
+               .capture();
+}
+
 int main()
 {
     std::cout << "jg_stacktrace sample...\n\n";
 
-    const auto trace = jg::stack_trace()
-                       .include_frame_count(10) // can be less
-                       .skip_frame_count(1) // skips main()
-                       .capture();
-
-    for (const auto& frame : trace) 
+    for (const auto& frame : get_trace()) 
         std::cout << frame << "\n";
     
     std::cout << "\n...done";
