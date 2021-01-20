8c4fcd783d20051fa00ba717c8c680471c176e5d
Johann Gerell <johann@gerell.net>
2021-01-20 18:56:20 +0100

    Added new features

--- inc/jg_string.h
+++ inc/jg_string.h
@@ -1,7 +1,10 @@
 #pragma once
 
-#include <string>
+#include <array>
 #include <cstring>
+#include <charconv>
+#include <optional>
+#include <string>
 #include "jg_span.h"
 #include "jg_verify.h"
 
@@ -21,6 +24,49 @@ inline std::string& trim_right(std::string& string, const std::string& chars = "
 inline std::string& trim(std::string& string, const std::string& chars = "\t\n\v\f\r ")
 {
     return trim_left(trim_right(string, chars), chars);
+}
+
+constexpr bool starts_with(std::string_view string, std::string_view start)
+{
+    return string.rfind(start, 0) == 0;
+}
+
+template <size_t NumTokens>
+constexpr std::optional<std::array<std::string_view, NumTokens>> split(std::string_view string, char delimiter)
+{
+    static_assert(NumTokens > 0);
+
+    std::array<std::string_view, NumTokens> tokens;
+
+    for (size_t i = 0; i < NumTokens; ++i)
+    {
+        const size_t offset = string.find(delimiter);
+
+        if (offset != std::string_view::npos && i == NumTokens - 1)
+            return std::nullopt;
+
+        if (offset == std::string_view::npos && i != NumTokens - 1)
+            return std::nullopt;
+
+        tokens[i] = string.substr(0, offset);
+
+        if (offset != std::string_view::npos)
+            string.remove_prefix(offset + 1);
+    }
+
+    return tokens;
+}
+
+template <typename T, typename U = T>
+constexpr std::optional<U> from_chars(std::string_view string)
+{
+    T value{};
+    auto [_, result] = std::from_chars(string.data(), string.data() + string.size(), value);
+
+    if (result == std::errc())
+        return static_cast<U>(value);
+
+    return std::nullopt;
 }
 
 class string_ref
