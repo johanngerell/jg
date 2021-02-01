fce3c33621fe79c1b64ab612f5e363ab69589b6a
Johann Gerell <johann.gerell@king.com>
2021-02-01 18:16:35 +0100

    Tidy-up of stopwatch

--- inc/jg_stopwatch.h
+++ inc/jg_stopwatch.h
@@ -5,34 +5,30 @@
 namespace jg
 {
 
+/// The simplest possible .Net StopWatch rip-off. Continues to measure time from instantiation,
+/// regardless of how many times ms, us or ns are called. Restarts when assigned a new instance.
+///
+/// @example
+///     stopwatch sw; // starts timing
+///     op1();
+///     std::cout << "op1() took " << sw.ns() << " ns\n";
+///     sw = {}; // "restarts" timing
+///     op2();
+///     std::cout << "op2() took " << sw.ms() << " ms\n";
 class stopwatch final
 {
 public:
-    std::chrono::microseconds::rep us() const
-    {
-        return duration_rep<std::chrono::microseconds>();
-    }
-
-    std::chrono::milliseconds::rep ms() const
-    {
-        return duration_rep<std::chrono::milliseconds>();
-    }
-
-    void restart()
-    {
-        m_start = clock::now();
-    }
+    std::chrono::milliseconds::rep ms() const { return rep<std::chrono::milliseconds>(); }
+    std::chrono::microseconds::rep us() const { return rep<std::chrono::microseconds>(); }
+    std::chrono::nanoseconds::rep  ns() const { return rep<std::chrono::nanoseconds>(); }
 
 private:
     using clock = std::chrono::high_resolution_clock;
 
     template <typename T>
-    typename T::rep duration_rep() const
-    {
-        return std::chrono::duration_cast<T>(clock::now() - m_start).count();
-    }
+    typename T::rep rep() const { return std::chrono::duration_cast<T>(clock::now() - m_start).count(); }
     
-    clock::time_point m_start = clock::now();
+    clock::time_point m_start{clock::now()};
 };
 
 } // namespace jg
