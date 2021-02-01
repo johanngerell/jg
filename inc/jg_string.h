e7a276ec49bcbba7fe716a8db03b4a9c971dc559
Johann Gerell <johann.gerell@king.com>
2021-02-01 17:58:21 +0100

    Embryo for fixed_string

--- inc/jg_string.h
+++ inc/jg_string.h
@@ -83,128 +83,22 @@ constexpr std::optional<U> from_chars(std::string_view string)
     return std::nullopt;
 }
 
-#if 0
-
-class string_ref
-{
-public:
-    constexpr string_ref() = default;
-
-    constexpr string_ref(const char* string)
-        : m_string{string, strlen(string)}
-    {}
-
-    constexpr string_ref(const char* string, size_t length)
-        : m_string{string, length}
-    {}
-
-    template <size_t N>
-    constexpr string_ref(const char (&string)[N])
-        : m_string{string, N - 1}
-    {
-        // Don't want char arrays with embedded nulls.
-        debug_verify(strlen(string) == N - 1);
-    }
-
-    template <size_t N>
-    constexpr static string_ref literal(const char (&string)[N])
-    {
-        // Don't want char arrays with embedded nulls.
-        debug_verify(strlen(string) == N - 1);
-        return {string, N - 1};
-    }
-
-    string_ref find(const char* string) const
-    {
-        if (const char* match = strstr(c_str(), verified(string)))
-            return {match, length() - (match - c_str())};
-        else
-            return {};
-    }
-
-    constexpr explicit operator bool() const
-    {
-        return m_string.operator bool();
-    }
-
-    constexpr const char* c_str() const { return m_string.data(); }
-    constexpr size_t length() const { return m_string.size() - 1; }
-
-private:
-    span<const char> m_string;
-};
-
-inline bool operator==(const string_ref& first, const string_ref& second)
-{
-    return strcmp(first.c_str(), second.c_str()) == 0;
-}
-
-inline bool operator!=(const string_ref& first, const string_ref& second)
-{
-    return !(first == second);
-}
-
-template <size_t MAX_LENGTH>
+template <size_t SIZE_WITH_NUL>
 class fixed_string final
 {
 public:
-    constexpr fixed_string(const char* string, size_t length)
+    constexpr fixed_string(const char (&string)[SIZE_WITH_NUL])
     {
-        verify(length <= MAX_LENGTH);
-        strncpy(m_string, verified(string), length + 1);
-        m_string[length] = '\0';
+        static_assert(string[SIZE_WITH_NUL - 1] == '\0');
+
+        for (size_t i = 0; i < SIZE_WITH_NUL; ++i)
+            m_string[i] = string[i];
     }
 
-    constexpr fixed_string(const char* string)
-        : fixed_string(string, strlen(verified(string)))
-    {
-    }
-
-    constexpr const char* c_str() const { return m_string; }
-    constexpr size_t length() const { return strlen(m_string); }
-
-    constexpr explicit operator string_ref() const { return string_ref{c_str(), length()}; }
+    operator std::string_view() const { return {m_string, SIZE_WITH_NUL - 1}; }
 
 private:
-    char m_string[MAX_LENGTH + 1];
+    char m_string[SIZE_WITH_NUL];
 };
-
-template <size_t M, size_t N>
-inline bool operator==(const fixed_string<M>& first, const fixed_string<N>& second)
-{
-    return strcmp(first.c_str(), second.c_str()) == 0;
-}
-
-template <size_t M, size_t N>
-inline bool operator!=(const fixed_string<M>& first, const fixed_string<N>& second)
-{
-    return !(first == second);
-}
-
-template <size_t N>
-inline bool operator==(const string_ref& first, const fixed_string<N>& second)
-{
-    return strcmp(first.c_str(), second.c_str()) == 0;
-}
-
-template <size_t N>
-inline bool operator!=(const string_ref& first, const fixed_string<N>& second)
-{
-    return !(first == second);
-}
-
-template <size_t N>
-inline bool operator==(const fixed_string<N>& first, const string_ref& second)
-{
-    return strcmp(first.c_str(), second.c_str()) == 0;
-}
-
-template <size_t N>
-inline bool operator!=(const fixed_string<N>& first, const string_ref& second)
-{
-    return !(first == second);
-}
-
-#endif
 
 } // namespace jg
