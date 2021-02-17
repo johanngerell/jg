82b1e2e713b048a2acc9b8fd3d8c5c01c2201c8d
Johann Gerell <johann@gerell.net>
2021-02-17 11:37:42 +0100

    Removed ctime_safe as timestamp formatting doesn't use it any more

--- inc/jg_os.h
+++ inc/jg_os.h
@@ -9,10 +9,6 @@
 
 namespace jg::os {
 
-/// Formats a time string in the format "Www Mmm dd hh:mm:ss yyyy\n", with individual fixed-length fields.
-/// This string has length 25, which means that `size` must be at least size 26 to fit the null-terminator.
-bool ctime_safe(char* buffer, size_t size, time_t time);
-
 tm* localtime_safe(time_t time, tm& result);
 
 } // namespace jg::os
@@ -21,21 +17,6 @@ tm* localtime_safe(time_t time, tm& result);
 #undef JG_OS_IMPL
 
 namespace jg::os {
-
-bool ctime_safe(char* buffer, size_t size, time_t time)
-{
-    // ctime_s and ctime_r format a string exactly in the format "Www Mmm dd hh:mm:ss yyyy\n", with
-    // individual fixed-length fields. This string has length 25, which means that the target buffer
-    // must be at least size 26 to fit the null-terminator.
-    if (size < sizeof("Www Mmm dd hh:mm:ss yyyy\n"))
-        return false;
-
-#ifdef _WIN32
-    return ctime_s(buffer, size, &time) == 0;
-#else
-    return ctime_r(&time, buffer) != nullptr;
-#endif
-}
 
 tm* localtime_safe(time_t time, tm& result)
 {
