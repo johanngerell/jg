71183590bb450ed996f73b6652eb38ab41376817
Johann Gerell <johann.gerell@king.com>
2021-02-18 14:07:41 +0100

    Simpler test suites auto discovery using test_adder

--- tests/simple_logger_tests.cpp
+++ tests/simple_logger_tests.cpp
@@ -31,27 +31,21 @@ jg::timestamp make_timestamp(size_t hours, size_t minutes, size_t seconds, size_
     return {mktime(&tm), static_cast<decltype(jg::timestamp::milliseconds)>(milliseconds)};
 }
 
-struct simple_logger_tests final
-{
-    simple_logger_tests()
-    {
-        jg::test_add(jg::test_suites { "simple_logger", {
-            jg::test_suite { "to_string", {
-                jg::test_case { "make_timestamp should fail on too big values", [] {
-                    jg_test_assert_exception(make_timestamp(24,  1,  1,    1), std::range_error);
-                    jg_test_assert_exception(make_timestamp( 1, 60,  1,    1), std::range_error);
-                    jg_test_assert_exception(make_timestamp( 1,  1, 60,    1), std::range_error);
-                    jg_test_assert_exception(make_timestamp( 1,  1,  1, 1000), std::range_error);
-                }},
-                jg::test_case { "to_string", [] {
-                    jg_test_assert(jg::to_string(make_timestamp(0, 0, 0, 0)) == "00:00:00.000");
-                    jg_test_assert(jg::to_string(make_timestamp(1, 1, 1, 1)) == "01:01:01.001");
-                    jg_test_assert(jg::to_string(make_timestamp(16, 35, 12, 123)) == "16:35:12.123");
-                    jg_test_assert(jg::to_string(make_timestamp(23, 59, 59, 999)) == "23:59:59.999");
-                }}
-            }}
-        }});
-    }
-} _;
+jg::test_adder simple_logger_tests { "simple_logger", {
+    jg::test_suite { "to_string", {
+        jg::test_case { "make_timestamp should fail on too big values", [] {
+            jg_test_assert_exception(make_timestamp(24,  1,  1,    1), std::range_error);
+            jg_test_assert_exception(make_timestamp( 1, 60,  1,    1), std::range_error);
+            jg_test_assert_exception(make_timestamp( 1,  1, 60,    1), std::range_error);
+            jg_test_assert_exception(make_timestamp( 1,  1,  1, 1000), std::range_error);
+        }},
+        jg::test_case { "to_string", [] {
+            jg_test_assert(jg::to_string(make_timestamp(0, 0, 0, 0)) == "00:00:00.000");
+            jg_test_assert(jg::to_string(make_timestamp(1, 1, 1, 1)) == "01:01:01.001");
+            jg_test_assert(jg::to_string(make_timestamp(16, 35, 12, 123)) == "16:35:12.123");
+            jg_test_assert(jg::to_string(make_timestamp(23, 59, 59, 999)) == "23:59:59.999");
+        }}
+    }}
+}};
 
 }
